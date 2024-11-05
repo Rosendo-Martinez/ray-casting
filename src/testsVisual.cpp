10b71907831f303066ef2d42cf59495858667990
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-11-05 14:43:08 -0600

    fuck me

--- src/testsVisual.cpp
+++ src/testsVisual.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "RayCasting.h"
+#include <iostream>
 
 // g++ -c visual.cpp
 // g++ visual.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
@@ -48,6 +49,19 @@ void drawLineSegment(LineSegment ls, sf::RenderWindow & window)
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1200, 800), "Window");
+    window.setKeyRepeatEnabled(false);
+
+    std::vector<Point> points;
+    std::vector<LineSegment> lineSegments;
+    std::vector<Line> lines;
+
+    points.push_back(Point(100,100));
+    lines.push_back(Line(PI/2, Point(201,0)));
+    lines.push_back(Line(PI - 0.5, Point(100, 500)));
+    lines.push_back(Line(0.5, Point(50,50)));
+    lineSegments.push_back(LineSegment(Point(50,120), Point(400,700)));
+
+    float scale = 1;
 
     while (window.isOpen())
     {
@@ -55,14 +69,66 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Up)
+                {
+                    scale += .10f;
+
+                    if (scale > 4.f)
+                    {
+                        scale = 4.f;
+                    }
+                    std::cout << "Scale: " << scale << "\n";
+                }
+                if (event.key.code == sf::Keyboard::Down)
+                {
+                    scale -=.10f;
+
+                    if (scale < .1f)
+                    {
+                        scale = .1f;
+                    }
+
+                    std::cout << "Scale: " << scale << "\n";
+                }
+            }
         }
 
         window.clear();
-        drawPoint(Point(100,100), window);
-        drawLine(Line(PI/2, Point(201,0)), window);
-        drawLine(Line(PI - 0.5, Point(100, 500)), window);
-        drawLineSegment(LineSegment(Point(50,120), Point(400,700)), window);
+
+        for (auto p : points)
+        {
+            drawPoint(Point(p.x * scale, p.y * scale), window);
+        }
+
+        for (auto ls : lineSegments)
+        {
+            drawLineSegment(LineSegment(Point(ls.a.x * scale, ls.a.y * scale), Point(ls.b.x * scale, ls.b.y * scale)), window);
+        }
+
+        for (auto l : lines)
+        {
+            LineSegment ls;
+
+            if (l.type() == VERTICAL)
+            {
+                ls.a = Point(l.p.x * scale, 0);
+                ls.b = Point(l.p.x * scale, window.getSize().y);
+            }
+            else
+            {
+                ls.a = Point(0, l.f(0) * scale);
+                ls.b = Point(window.getSize().x * scale, l.f(window.getSize().x) * scale);
+            }
+
+            drawLineSegment(ls, window);
+        }
+
         window.display();
     }
 
