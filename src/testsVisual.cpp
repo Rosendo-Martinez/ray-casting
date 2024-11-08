b387b60f7c37899f49dd61fb2394045f9906cee5
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-11-07 18:54:29 -0600

    Clean up render()

--- src/testsVisual.cpp
+++ src/testsVisual.cpp
@@ -19,7 +19,7 @@ private:
     std::vector<Point> intersectionPoints;
     std::vector<LineSegment> intersectionLineSegments;
     std::vector<Point> fan;
-    Point rayBase = Point(7,5);
+    Point rayBase = Point(7,9);
     bool isHoldingRightClick = false;
 
     Point scale(const Point p) const
@@ -140,8 +140,9 @@ private:
         map.push_back(lj);
 
 
-        rays = getIntersectionsOfRays(rayBase, 10, map, intersectionPoints, intersectionLineSegments);
-        getClosestIntersectionsOfRays(rayBase, 100, map, fan);
+        rays = getIntersectionsOfRays(rayBase, 25, map, intersectionPoints, intersectionLineSegments);
+        getClosestIntersectionsOfRays(rayBase, 25, map, fan);
+        std::cout << "Inter Line Seg Size: " << intersectionLineSegments.size() << "\n";
     }
 
     void drawPoint(Point p, sf::Color color = sf::Color::White)
@@ -189,8 +190,8 @@ private:
             // Descale mouse pos and window pos
             rayBase = Point(sf::Mouse::getPosition(window).x/m_scale + windowPOS.x/m_scale, sf::Mouse::getPosition(window).y/m_scale + windowPOS.y/m_scale);
 
-            rays = getIntersectionsOfRays(rayBase, 100, map, intersectionPoints, intersectionLineSegments);
-            getClosestIntersectionsOfRays(rayBase, 100, map, fan);
+            rays = getIntersectionsOfRays(rayBase, 25, map, intersectionPoints, intersectionLineSegments);
+            getClosestIntersectionsOfRays(rayBase, 25, map, fan);
         }
 
         sf::Event event;
@@ -284,62 +285,53 @@ private:
         window.clear();
 
         // Draw axis
-        // drawPoint(scale(Point(0,0)));
         drawLineSegment(toDrawableLineSegment(scale(Line(0, Point(0,0)))), sf::Color(128,128,128, 255));
         drawLineSegment(toDrawableLineSegment(scale(Line(PI/2, Point(0,0)))), sf::Color(128,128,128, 255));
-        // drawLineSegment(toDrawableLineSegment(scale(Ray(PI/4, Point(100,100)))));
-        // drawPoint(scale(Point(100,100)));
 
-        // std::vector<Point> points;
-        // std::vector<LineSegment> lineSegments;
-        // std::vector<Line> lines;
-
+        // Draw map
         for (auto ls : map)
         {
             drawLineSegment(scale(ls));
         }
 
+        // Draw rays
         // for (auto r : rays)
         // {
-        //     drawLineSegment(toDrawableLineSegment(scale(r)), sf::Color(255, 255, 0, 100));
+        //     drawLineSegment(toDrawableLineSegment(scale(r)), sf::Color(255, 255, 0, 50));
         // }
 
+        // Draw light source
         drawPoint(scale(rayBase), sf::Color(255,255,0,255));
 
-        // Size is fan size plus 1 for ray base
-        sf::VertexArray triangleFan(sf::TriangleFan, fan.size() + 2);
+        // Draw fan and closest intersection points
+        sf::VertexArray triangleFan(sf::TriangleFan, fan.size() + 2); // +2 for light source point, and for repeating first point on fan
         triangleFan[0] = sf::Vector2f(scale(rayBase).x, scale(rayBase).y) - windowPOS;
         triangleFan[0].color = sf::Color(255,255,0,100);
-
         for (int i = 0; i < fan.size(); i++)
         {
             triangleFan[i + 1].position = sf::Vector2f(scale(fan[i]).x,scale(fan[i]).y) - windowPOS;
-
             triangleFan[i + 1].color = sf::Color(255,255,0,50);
         }
-
         // double up on first point in fan vector to complete actual fan (i.e make it go fully around)
         triangleFan[fan.size() + 1].position = sf::Vector2f(scale(fan[0]).x,scale(fan[0]).y) - windowPOS;
         triangleFan[fan.size() + 1].color = sf::Color(255,255,0,50);
-
         window.draw(triangleFan);
-
         for (auto p : fan)
         {
             drawPoint(scale(p), sf::Color(255, 0, 0, 200));
             drawLineSegment(scale(LineSegment(rayBase, p)), sf::Color(255, 255, 0, 100));
         }
 
-
-
+        // Draw all intersection points
         // for (auto p : intersectionPoints)
         // {
         //     drawPoint(scale(p), sf::Color(255, 0, 0, 200));
         // }
 
+        // Draw all intersection line segments
         // for (auto ls : intersectionLineSegments)
         // {
-        //     drawLineSegment((scale(ls)));
+        //     drawLineSegment(scale(ls), sf::Color(255,0,0,200));
         // }
 
         window.display();
